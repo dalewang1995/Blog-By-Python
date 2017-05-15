d95d74e12519f4462071e68abb2edb3d69faaae5
dalewang1995 <dalewang1995@gmail.com>
2017-05-15 11:18:07 +0800

    网络部署，配置 WSGI

--- wsgi/django.wsgi.py
+++ wsgi/django.wsgi.py
@@ -4,11 +4,7 @@ from django.core.wsgi import get_wsgi_application
 
 # Add this file path to sys.path in order to import settings
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Blog.settings'
-
 sys.stdout = sys.stderr
-
 DEBUG = True
-
 application = get_wsgi_application()
